1b1d190d3352650b65a9bee53f007845a3b1b761
acabStephan <101325292+acabStephan@users.noreply.github.com>
2022-04-21 11:24:52 +0200

    Serialization done, ready to save into data.json

--- GetFiles/GetFiles/Program.cs
+++ GetFiles/GetFiles/Program.cs
@@ -3,9 +3,23 @@ using System.Text.Json;
 
 var swp = new Subject("Softwareentwicklung", "C:/Users/Stephan/OneDrive - HTL Neufelden");
 
-var strJson = JsonSerializer.Serialize<Subject>(swp);
-Console.WriteLine(strJson);
+//var strJson = JsonSerializer.Serialize<Subject>(swp);
+//Console.WriteLine(strJson);
 
-var nonJson = JsonSerializer.Deserialize<Subject>(strJson);
-Console.WriteLine(nonJson.GetType());
-Console.WriteLine(nonJson.Name);
+//var nonJson = JsonSerializer.Deserialize<Subject>(strJson);
+//Console.WriteLine(nonJson.GetType());
+//Console.WriteLine(nonJson.Name);
+
+var subjectList = new List<Subject>();
+subjectList.Add(swp);
+subjectList.Add(new Subject("Labor", "C:/Users/Stephan/OneDrive - HTL Neufelden"));
+
+var serializedSubjectList = JsonSerializer.Serialize<Subject[]>(subjectList.ToArray());
+var deserializedSubjList = JsonSerializer.Deserialize<Subject[]>(serializedSubjectList);
+//Console.WriteLine(deserializedSubjList[1].Name);
+
+await File.WriteAllTextAsync("C:/Users/Stephan/data.json", serializedSubjectList);
+var readData = await File.ReadAllTextAsync("C:/Users/Stephan/data.json");
+var NonJsonReadData = JsonSerializer.Deserialize<Subject[]>(readData);
+Console.WriteLine(NonJsonReadData[1].Name);
+Console.WriteLine(NonJsonReadData[0].Name);
